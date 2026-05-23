19a4609c49ff2c1699c87007cbf34d9ad7278680
Marcelo-vta <mclonso123@outlook.com>
2026-05-23 20:47:59 -0300

    fix: jwt duration declaring typo

--- src/main/java/store/auth/AuthService.java
+++ src/main/java/store/auth/AuthService.java
@@ -13,11 +13,11 @@ import store.account.AccountOut;
 public class AuthService {
 
     // Business logic
-    
+
     // Delegates registration to account
     // Generates tokens by calling JwtService
     
-    @Value("${store.jwt.duration")
+    @Value("${store.jwt.duration}")
     private Long duration;
     
     @Value("${store.jwt.httpOnly}")
